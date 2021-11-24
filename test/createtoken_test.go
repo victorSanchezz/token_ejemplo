68c62c21bc4b10468b528ccc85446f37e8dd85d8
victorSanchezz <94009704+victorSanchezz@users.noreply.github.com>
2021-11-24 13:22:54 -0600

    test

--- test/createtoken_test.go
+++ test/createtoken_test.go
@@ -1,9 +1,22 @@
 package test
 
 import (
+	"jwt-todo/estructura"
 	"testing"
 )
 
-func TestCreateToken(t *testing.T) {
-	
+func TestCreateTokenInvalido(t *testing.T) {
+	idinvalido := uint64(2)
+	id := estructura.User1.ID
+	if id != idinvalido {
+		t.Fatalf("error con el id %v, se esperaba que fuera %v", idinvalido, id)
+	}
+}
+
+func TestCreateTokenValido(t *testing.T) {
+	invalido := uint64(1)
+	id := estructura.User1.ID
+	if id != invalido {
+		t.Fatalf("error con el id %v, se esperaba que fuera %v", invalido, id)
+	}
 }
--- test/login_test.go
+++ test/login_test.go
@@ -1,9 +1,34 @@
 package test
 
 import (
+	"jwt-todo/estructura"
 	"testing"
 )
 
-func TestLogin(t *testing.T) {
+func TestLoginValido(t *testing.T) {
+	Username := "username"
+	Password := "password"
+
+	if estructura.User1.Username != Username {
+		t.Fatal("se esperaba que el usuario fuera invalido")
+	}
+
+	if estructura.User1.Password != Password {
+		t.Fatal("se esperaba que la contraseña fuera invalida")
+	}
+
+}
+
+func TestLoginInvalido(t *testing.T) {
+	Username := "user"
+	Password := "pass"
+
+	if estructura.User1.Username != Username {
+		t.Fatalf("el username: %s es invalido, se esperaba %s", Username, estructura.User1.Username)
+	}
+
+	if estructura.User1.Password != Password {
+		t.Fatalf("el password: %s es invalido, se esperaba %s", Password, estructura.User1.Password)
+	}
 
 }
