78c9e2641a714c8775b9517a4bd380633629ca82
victorSanchezz <94009704+victorSanchezz@users.noreply.github.com>
2021-11-25 10:19:57 -0600

    Update tokenverification_test.go

--- test/tokenverification_test.go
+++ test/tokenverification_test.go
@@ -30,7 +30,8 @@ func TestTokenValidValid(t *testing.T) {
 	}
 }
 func TestTokenValidInvalid(t *testing.T) {
-	tooken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJlbWFpbCI6Impvc2VAZ21haWwuY2s9tIiwiZXhwIjoxNjM3NzgyOTE5LCJsYXNuYW1lIjoiam9zZSIsIm5hbWUiOiJqb3NlIiwicGhvbmUiOiIxMjM0NTY3ODkwIiwidXNlcl9pZCI6MX0.pjwgVIWmXva5Mpc_aAViCIAm8PQOfcHtOhMj3MvHzi8"
+	//tooken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJlbWFpbCI6Impvc2VAZ21haWwuY2s9tIiwiZXhwIjoxNjM3NzgyOTE5LCJsYXNuYW1lIjoiam9zZSIsIm5hbWUiOiJqb3NlIiwicGhvbmUiOiIxMjM0NTY3ODkwIiwidXNlcl9pZCI6MX0.pjwgVIWmXva5Mpc_aAViCIAm8PQOfcHtOhMj3MvHzi8"
+	tooken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJlbWFpbCI6Impvc2VAZ21haWwuY29tIiwiZXhwIjoxNjM3Nzg3NzM2LCJsYXNuYW1lIjoiam9zZSIsIm5hbWUiOiJqb3NlIiwicGhvbmUiOiIxMjM0NTY3ODkwIiwidXNlcl9pZCI6MX0.XtVL2Vj5c1tuSK9ENf65L-9W7Jo49ji9FDN4M6pC2aM"
 	claims := &Claims{}
 	tkn, err := jwt.ParseWithClaims(tooken, claims, func(token *jwt.Token) (interface{}, error) {
 		return jwtKey, nil
